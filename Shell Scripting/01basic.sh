9ce31eaa2cb12f92e4ca080f9e1f7c5fcac84424
Ujjwal <ujjwalkamila86@gmail.com>
2024-05-13 22:46:40 +0530

    Shell Scripting basics

---
+++ Shell Scripting/01basic.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#For printing something use echo cmd
+echo "Hello World!"
+
+echo "Hello Buddy!"
+
+#check my shell type run 
+# echo $0
+
+# to see permission run
+# ls -ltr 
+
+# to give permission run
+# chmod
+
+#to see or read the code run
+# cat script.sh
+
+# Run using
+
+# ./script.sh
+# /path/script.sh
+# bash script.sh
+
