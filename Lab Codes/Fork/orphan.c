7f15b3fa39d2e9a437e462f21e2fe842d16a0b6b
Ujjwal <ujjwalkamila86@gmail.com>
2024-06-11 23:53:44 +0530

    Lab codes "How to remove the Parent process "

---
+++ Lab Codes/Fork/orphan.c
@@ -0,0 +1,20 @@
+#include<unistd.h>
+#include<sys/types.h>
+#include<stdio.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+
+int main()
+{
+    pid_t t;
+    t = fork();
+    if(t == 0){
+        printf("Child having id %d\n", getpid());
+        sleep(10); 
+        printf("I'm Living");
+    } else {
+        printf("Parent having id %d\n", getpid());
+        exit(0); 
+    }
+    return 0;
+}
