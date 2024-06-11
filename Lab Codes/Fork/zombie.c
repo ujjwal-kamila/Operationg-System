360030a0002130ebf24e3ccb401f35d123df4077
Ujjwal <ujjwalkamila86@gmail.com>
2024-06-11 23:50:33 +0530

    Lab codes  "Creating a Zombie Process using fork "

---
+++ Lab Codes/Fork/zombie.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main (){
+	pid_t t;
+	t = fork();
+	if (t == 0){
+		printf("child have id %d\n", getpid());
+	}
+	else{
+		printf("Parent have id %d\n",getpid());
+		sleep(1000);
+
+	}
+    return 0;
+}
